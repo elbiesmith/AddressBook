5af72334607ba8b941c608edf69550a2341b7289
elbiesmith <elbie.smith@gmail.com>
2021-08-02 15:51:32 -0500

    Initial commit

---
+++ Data/Migrations/20210729181037_DbModelUpdate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AddressBook.Data.Migrations
+{
+    public partial class DbModelUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Updated",
+                table: "Contacts",
+                type: "timestamp without time zone",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Updated",
+                table: "Contacts");
+        }
+    }
+}
